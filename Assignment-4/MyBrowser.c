0f3aefd4c26ae0da2e2a19d45768559121b403bd
Gaurav Malakar <varuag2002@gmail.com>
2023-02-09 12:28:19 +0530

    minor changes

--- Assignment-4/MyBrowser.c
+++ Assignment-4/MyBrowser.c
@@ -14,34 +14,33 @@
 #define BADREQUEST 400
 #define FORBIDDEN 403
 
-#define LOCAL "127.0.0.1"
+#define LOCALHOST "127.0.0.1"
 #define MAXLINE 10
 #define MAXCONNECTIONS 5
 #define DURATION 5000
+
+void get(char *url);                    // implements GET {url}
+void put(char *url, char *filename);    // Implements PUT {url} <filename>
 
 int main()
 {
     // assumed url max size
     char cmd[4], url[100], filename[100];
-    while(1)
-    {
+    while(1){
         printf("MyOwnBrowser> ");
         scanf("%s, %s", cmd, url);
-        if(!strcmp(cmd, "GET"))
-        {
-
+        if(strcmp(cmd, "GET")==0){
+            get(url);
         }
-        else if(!strcmp(cmd, "PUT"))
-        {
+        else if(strcmp(cmd, "PUT")==0){
             scanf("%s", filename);
+            put(url, filename);
         }
-        else if(!strcmp(cmd, "QUIT"))
-        {
+        else if(strcmp(cmd, "QUIT")==0){
             printf("Bye\n");
             break;
         }
-        else
-        {
+        else{
             printf("Invalid command\n");
         }
     }
