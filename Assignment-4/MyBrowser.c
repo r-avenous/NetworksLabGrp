b45784b80c075eb7f7afc14644805f03216212e8
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-02-12 11:17:20 +0530

    .

--- Assignment-4/MyBrowser.c
+++ Assignment-4/MyBrowser.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <poll.h>
@@ -19,46 +19,48 @@
 #define MAXCONNECTIONS 5
 #define DURATION 5000
 
-void get(char *url);                    // Implements GET {url}
-void put(char *url, char *filename);    // Implements PUT {url} <filename>
-void get_to_request(char *url, char *request); // converts get command to HTTP request
+void get(char *url);                                           // Implements GET {url}
+void put(char *url, char *filename);                           // Implements PUT {url} <filename>
+void get_to_request(char *url, char *request);                 // converts get command to HTTP request
 void set_content_type(const char *accept, char *content_type); // sets conent type according to accept header
 
 int main()
 {
 
-
     // assumed url max size
     char cmd[4], url[200], filename[100];
-    while(1){
+    while (1)
+    {
         printf("MyOwnBrowser> ");
 
-        scanf("%s", cmd); 
-        if(strcmp(cmd, "QUIT")==0){
+        scanf("%s", cmd);
+        if (strcmp(cmd, "QUIT") == 0)
+        {
             printf("Bye....\n");
             break;
         }
 
         scanf("%s", url);
-        if(strcmp(cmd, "GET")==0){
+        if (strcmp(cmd, "GET") == 0)
+        {
             get(url);
-            
         }
-        else if(strcmp(cmd, "PUT")==0){
+        else if (strcmp(cmd, "PUT") == 0)
+        {
             scanf("%s", filename);
             put(url, filename);
         }
-        else{
+        else
+        {
             printf("Invalid command\n");
         }
     }
     return 0;
 }
 
-
-
 // Implements GET {url}
-void get(char *url){
+void get(char *url)
+{
     char http_request[10000], server_ip[16];
 
     get_to_request(url, http_request);
@@ -71,41 +73,40 @@ void get(char *url){
 
     printf("\n\nRequest Sent=\n%s\n\n", http_request);
 
+    strcpy(server_ip, url + 7); // GET http://127.0.0.1:8000/
 
-    strcpy(server_ip, url+7);  // GET http://127.0.0.1:8000/ 
-
-    for(int i=0; i<16; i++){
-        if(server_ip[i]!='.' && (server_ip[i]<'0' || server_ip[i]>'9')){
-            server_ip[i]='\0';
+    for (int i = 0; i < 16; i++)
+    {
+        if (server_ip[i] != '.' && (server_ip[i] < '0' || server_ip[i] > '9'))
+        {
+            server_ip[i] = '\0';
             break;
         }
     }
     printf("SERVER IP = %s\n\n\n", server_ip);
 
-
     int sockfd;
-    struct sockaddr_in	serv_addr;
+    struct sockaddr_in serv_addr;
 
-    // Opening a new socket is exactly similar to the server process 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("Unable to create socket\n");
-      exit(0);
+    // Opening a new socket is exactly similar to the server process
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Unable to create socket\n");
+        exit(0);
     }
 
-
-    serv_addr.sin_family = AF_INET;	
+    serv_addr.sin_family = AF_INET;
     inet_aton(server_ip, &serv_addr.sin_addr);
-    serv_addr.sin_port	= htons(8050);
+    serv_addr.sin_port = htons(8050);
 
-    int status = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    if (status < 0) {
+    int status = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (status < 0)
+    {
         perror("Unable to connect to server\n");
         exit(0);
     }
 
-
-    
-    send(sockfd, http_request, strlen(http_request)+1, 0);
+    send(sockfd, http_request, strlen(http_request) + 1, 0);
 
     char response[100000];
     recv(sockfd, response, 10000, MSG_WAITALL);
@@ -114,20 +115,16 @@ void get(char *url){
     close(sockfd);
 
     return;
-}  
-
-
+}
 
 // Implements PUT {url} <filename>
-void put(char *url, char *filename){
+void put(char *url, char *filename)
+{
     return;
 }
 
-
-
-
-
-void get_to_request(char *url, char *request) {
+void get_to_request(char *url, char *request)
+{
 
     char *host, *path, *file_extension;
     time_t now;
@@ -137,20 +134,21 @@ void get_to_request(char *url, char *request) {
     char accept_language[100];
     char if_modified_since[100];
 
-
-    
     // get components of url
-    strsep(&url, "/"); strsep(&url, "/");
-    host=strsep(&url, "/");         // get host 
-    if(url) path=url;               // get file path
-    else path="/";
+    strsep(&url, "/");
+    strsep(&url, "/");
+    host = strsep(&url, "/"); // get host
+    if (url)
+        path = url; // get file path
+    else
+        path = "/";
 
     // get file ectension from path
-    file_extension=path;    
-    while(*file_extension != '.' && *file_extension != '\0'){
+    file_extension = path;
+    while (*file_extension != '.' && *file_extension != '\0')
+    {
         file_extension++;
     }
-
 
     // Get the current date and time
     time(&now);
@@ -158,14 +156,21 @@ void get_to_request(char *url, char *request) {
     strftime(date, sizeof(date), "%a, %d %b %Y %H:%M:%S %Z", tm);
 
     // Set the Accept header based on the file extension
-    if (strcmp(file_extension, ".html") == 0) {
-    strcpy(accept, "Accept: text/html\r\n");
-    } else if (strcmp(file_extension, ".pdf") == 0) {
-    strcpy(accept, "Accept: application/pdf\r\n");
-    } else if (strcmp(file_extension, ".jpg") == 0) {
-    strcpy(accept, "Accept: image/jpeg\r\n");
-    } else {
-    strcpy(accept, "Accept: */*\r\n");
+    if (strcmp(file_extension, ".html") == 0)
+    {
+        strcpy(accept, "Accept: text/html\r\n");
+    }
+    else if (strcmp(file_extension, ".pdf") == 0)
+    {
+        strcpy(accept, "Accept: application/pdf\r\n");
+    }
+    else if (strcmp(file_extension, ".jpg") == 0)
+    {
+        strcpy(accept, "Accept: image/jpeg\r\n");
+    }
+    else
+    {
+        strcpy(accept, "Accept: */*\r\n");
     }
 
     // Set the Accept-Language header
@@ -175,14 +180,10 @@ void get_to_request(char *url, char *request) {
     tm->tm_mday -= 2;
     strftime(if_modified_since, sizeof(if_modified_since), "If-Modified-Since: %a, %d %b %Y %H:%M:%S %Z\r\n", tm);
 
-
-
     // Construct the request message
     sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\nDate: %s\r\n%s%s%sConnection: close\r\n\r\n",
             path, host, date, accept, accept_language, if_modified_since);
-
 }
-
 
 /*
 GET http://127.0.0.1:8080/Hello.txt
