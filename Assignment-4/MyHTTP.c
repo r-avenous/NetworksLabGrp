cfce2ec58aa7b94e623cfd8b18a6efe00710b45b
Gaurav Malakar <varuag2002@gmail.com>
2023-02-12 18:16:28 +0530

    single file

--- Assignment-4/MyHTTP.c
+++ Assignment-4/MyHTTP.c
@@ -3,137 +3,74 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <poll.h>
 #include <time.h>
 
-#define NOTFOUND 404
-#define OK 200
-#define BADREQUEST 400
-#define FORBIDDEN 403
+#include "global_variables.h"
+#include "helper_functions.h"
 
-#define LOCALHOST "127.0.0.1"
-#define MAXLINE 10
-#define MAXCONNECTIONS 5
-#define DURATION 5000
-#define LOGGILE "AccessLog.txt"
-#define BUF_SIZE 10000
 
-int min( int a, int b){
-    if(a<b) return a;
-    return b;
-}
-void receive_request_headers(int sockfd, char *buf, int size){
-    const int PACKET_SIZE = 4;
-    int bytes_received = 0;
-    buf[0] = '\0';
-	int line_break=0, end_of_req=0;
-    while(bytes_received < size){
-        int bytes = recv(sockfd, buf + bytes_received, min(size - bytes_received, PACKET_SIZE), 0);
-        if(bytes == -1){
-            perror("Error in receiving data");
-            exit(0);
-        }
-        if(bytes == 0){
-            break;
-        }
+void implement_GET(char *path, char **values){
+	printf("\n--Hello--\n"); fflush(stdout);
+	printf("\n\nOpening: %s\n\n", path); fflush(stdout);
+	FILE *fp = fopen(path, "r");
+	if (fp == NULL) {
+		//File could not be opened
+		perror("Could not open file\n");
+		exit(EXIT_FAILURE);
+	}
 
-        // for(int i=0; i<bytes; ++i) printf("%d ", *(buf+bytes_received+i));
-        // printf("\n"); fflush(stdout);
-        // printf("%s", buf+bytes_received);
-
-		for(int i=0; i<bytes; ++i){
-			char ch = buf[bytes_received+i];
-			if(ch==' ' || ch=='\t' || ch=='\r')
-				continue;
-
-			if(ch=='\n'){
-				line_break++;
-				if(line_break==2){
-					end_of_req=1;
-					break;
-				}
-			}
-			else{
-				line_break=0;
-			}
-
-		}
-
-		if(end_of_req) break;
-
-        bytes_received += bytes;
-        if(buf[bytes_received-1] == '\0'){
-            break;
-        }
-        
-    }
+	char ch;
+    while ((ch = fgetc(fp)) != EOF) 
+        printf("%c", ch); 
+  
+    fclose(fp); 
 }
 
+int main(int argc, char **argv)
+{
+	int sockfd, newsockfd; // socket descriptors
+	struct sockaddr_in cli_addr; int clilen = sizeof(cli_addr); // client address, size
 
-int main(int argc, char **argv){
-	int	sockfd, newsockfd;		// socket descriptors 
-	int	clilen;
-	struct sockaddr_in	cli_addr, serv_addr;
-	int port_no = 8080;		// port number on which server accepts connections
+	int port_no = 8000; // default
+	if (argc > 1) port_no = atoi(argv[1]);
 
-	if(argc > 1){
-		port_no = atoi(argv[1]);
-	}
-	
+	char buf[BUF_SIZE]; // used to communicate with client
 
-	int i, status;				// status variable for bind() function
-	char buf[BUF_SIZE];		// used to communicate with client 		
-
-	// Opens a socket (returns -1 incase an error occurs )
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("Cannot create socket\n");
-		exit(0);
-	}
-
-	serv_addr.sin_family = AF_INET;			// the internet family
-	serv_addr.sin_addr.s_addr = INADDR_ANY;	// set to INADDR_ANY for machines having a single IP address
-	serv_addr.sin_port = htons(port_no);		// specifies the port number of the server
+	sockfd = startServer(port_no); // start the server
 
 
-	// bind() function to provide local address to the socket
-	status = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-	if (status< 0) {
-		perror("Unable to bind local address\n");
-		exit(0);
-	}
-
-	// Specifies that upto 5 active clients can wait for a connection
-	listen(sockfd, 5); 
-    printf("Server is listening on port %d....\n", port_no);
-
-	while(1) {
-		// accept() function waits until a client connects to the server and then sending/receiving pf data occurs
-		// newsockfd is used for communicating with client
-		clilen = sizeof(cli_addr);
-		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen); 
-
-		// if erroe in accept() then -1 is returned
-		if (newsockfd < 0) {
+	while (1)
+	{
+		newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+		if (newsockfd < 0){
 			perror("Accept error\n");
 			exit(0);
 		}
 
-        // receive the request from the client
-        receive_request_headers(newsockfd, buf, BUF_SIZE);
+		// receive the request from the client
+		receive_headers(newsockfd, buf, BUF_SIZE);
 
-        printf("Request received:\n %s\n", buf);
-        fflush(stdout);
+		printf("\n\nRequest received:\n%s\n", buf); fflush(stdout);
+
+		char **values;
+		char *method, *path;
+		parse_headers(buf, method, path, values);
+
+		printf("\n--Hello There--\n"); fflush(stdout);
+		printf("%s\n", method); fflush(stdout);
+		if(strcmp(method, "GET")==0){
+			//implement_GET(path, values);
+		}
+
 		
-		
-		send(newsockfd, "REQUEST RECEIVED", 17, 0);	// send the current time
+
+
+		// send(newsockfd, "REQUEST RECEIVED", 17, 0);	// send the current time
 		close(newsockfd);
-
 	}
 	return 0;
 }
-
-
-
--- Assignment-4/client.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get('http://localhost:8000')
-print(response.text)
