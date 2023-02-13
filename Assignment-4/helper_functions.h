22c21329c8cb211071efa412c959d8e92e03cb9c
Gaurav Malakar <varuag2002@gmail.com>
2023-02-13 18:17:59 +0530

    trivial

--- Assignment-4/helper_functions.h
+++ Assignment-4/helper_functions.h
@@ -246,3 +246,52 @@ void implement_GET(char *path, char **values, int newsockfd){
 	fclose(fp);
 
 }
+
+void receive_file_content(FILE *fp, int content_length, int newsockfd)
+{
+	// if there is any extra data in the buffer received along with the headers
+	fwrite(extra_data, 1, extra_data_size, fp);
+	content_length -= extra_data_size;
+	extra_data_size = 0;
+
+	char buf[BUF_SIZE];
+	int n;
+	printf("conntent_length: %d\n", content_length); fflush(stdout);
+	while(content_length>0){
+		n = recv(newsockfd, buf, MAXLINE, 0);
+		printf("\n***Received: %s\n", buf); fflush(stdout);
+		content_length -= n;
+		printf("conntent_length: %d\n", content_length); fflush(stdout);
+		fwrite(buf, 1, n, fp);
+	}
+
+}
+
+void implement_PUT(char *path, char **values, int newsockfd)
+{
+	// putting a '.' before the path
+	char *modified_path = (char *)malloc(sizeof(char)*(sizeof(path)+1));
+	strcpy(modified_path, "."); strcat(modified_path, path);
+
+	printf("\n\nReceiving : %s\n\n", modified_path); fflush(stdout);
+
+	FILE *fp = fopen(modified_path, "w");
+	if (fp == NULL) {
+		//File could not be opened(Probably not found)
+		perror("Could not open file\n");
+		// send_general_response(404, newsockfd);
+		// fp = fopen("404.html", "r");
+		// send_file(fp, "404.html", newsockfd);
+		return;
+	}
+
+	// receive the file content
+	int content_length = atoi(values[3]);	// "content-length:"  is at 3rd index
+	receive_file_content(fp, content_length, newsockfd);
+	fclose(fp);
+
+
+
+	// File Received
+	send_general_response(200, newsockfd);
+}
