504b343eb598a82d7333a14494e524f694942143
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2023-04-03 15:34:07 +0530

    Co-authored-by: gXurav07 <gXurav07@users.noreply.github.com>

--- Assignment-6/_pingnetinfo.c
+++ Assignment-6/_pingnetinfo.c
@@ -16,15 +16,18 @@ unsigned short in_cksum(unsigned short *ptr, int nbytes);
 void receive_packet(int sockfd);
 void send_packet(int sockfd, char *ip_addr, char *data);
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[]) 
+{
+    if (argc < 2) 
+    {
         printf("Usage: %s <ip_address>\n", argv[0]);
         return 1;
     }
 
 
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (sockfd < 0) {
+    if (sockfd < 0) 
+    {
         perror("socket");
         return 1;
     }
@@ -45,8 +48,9 @@ int main(int argc, char *argv[]) {
     receive_packet(sockfd);
 }
 
-void setICMP(struct icmp *icmp_hdr, char *data, int size) {
-    icmp_hdr->icmp_type = ICMP_ECHO;
+void setICMP(struct icmp *icmp_hdr, char *data, int size) 
+{
+    icmp_hdr->type = ICMP_ECHO;
     icmp_hdr->icmp_code = 0;
     icmp_hdr->icmp_id = getpid();
     icmp_hdr->icmp_seq = 1;
@@ -55,14 +59,15 @@ void setICMP(struct icmp *icmp_hdr, char *data, int size) {
     icmp_hdr->icmp_cksum = in_cksum((unsigned short *)icmp_hdr, size);    // 20 is ip header size
 }
 
-void setIP(struct iphdr *ip_hdr, struct sockaddr_in *dest_addr) {
+void setIP(struct iphdr *ip_hdr, struct sockaddr_in *dest_addr, int ttl) 
+{
     ip_hdr->ihl = 5;
     ip_hdr->version = 4;
     ip_hdr->tos = 0;
     ip_hdr->tot_len = htons(PACKET_SIZE);
     ip_hdr->id = htons(0);
     ip_hdr->frag_off = htons(0);
-    ip_hdr->ttl = 2;
+    ip_hdr->ttl = ttl;
     ip_hdr->protocol = IPPROTO_ICMP;
     ip_hdr->check = 0;
     ip_hdr->saddr = INADDR_ANY;
@@ -72,7 +77,8 @@ void setIP(struct iphdr *ip_hdr, struct sockaddr_in *dest_addr) {
 }
 
 
-void send_packet(int sockfd, char *ip_addr, char *data) {
+void send_packet(int sockfd, char *ip_addr, char *data) 
+{
     struct hostent *he = gethostbyname(ip_addr);
     if (he == NULL) {
         perror("gethostbyname"); return;
@@ -88,7 +94,7 @@ void send_packet(int sockfd, char *ip_addr, char *data) {
 
     char packet[PACKET_SIZE];
     struct iphdr *ip_hdr = (struct iphdr *)packet;
-    setIP(ip_hdr, &dest_addr);
+    setIP(ip_hdr, &dest_addr, 4);
 
     struct icmp *icmp_hdr = (struct icmp *)(packet+4*ip_hdr->ihl);
     setICMP(icmp_hdr, data, PACKET_SIZE-4*ip_hdr->ihl);
@@ -101,7 +107,8 @@ void send_packet(int sockfd, char *ip_addr, char *data) {
 
 }
 
-void receive_packet(int sockfd) {
+void receive_packet(int sockfd) 
+{
     // struct timeval timeout;
     // timeout.tv_sec = 3;
     // timeout.tv_usec = 0;
@@ -128,7 +135,8 @@ void receive_packet(int sockfd) {
 
 }
 
-unsigned short in_cksum(unsigned short *ptr, int nbytes) {
+unsigned short in_cksum(unsigned short *ptr, int nbytes) 
+{
     unsigned long sum;
     unsigned short oddbyte;
     unsigned short answer;
